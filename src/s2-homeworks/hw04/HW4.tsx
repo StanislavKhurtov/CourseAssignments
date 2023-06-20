4d2e7ca76cdd9bcf53e29a119a485f2e9404e8e3
Stanislav Khurtov <skhurtov.work@yandex.ru>
2023-06-20 13:45:10 +0300

    feat: add function onChangeCallback

--- src/s2-homeworks/hw04/HW4.tsx
+++ src/s2-homeworks/hw04/HW4.tsx
@@ -12,10 +12,10 @@ import Stand from './Stand'
 const HW4 = () => {
     return (
         <div id={'hw4'}>
-            <div className={s2.hwTitle}>Homework #4</div>
-            {/*демонстрация возможностей компонент:*/}
-            <div className={s2.hw}>
-                <Stand />
+            <div className={s2.container}>
+                <div className={s2.hwTitle}>Homework #4</div>
+                {/*демонстрация возможностей компонент:*/}
+                <div className={s2.hw}><Stand/></div>
             </div>
         </div>
     )
